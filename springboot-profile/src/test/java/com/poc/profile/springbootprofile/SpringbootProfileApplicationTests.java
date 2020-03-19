147bec8cfa66549c9b4cc0a423c9aa736bf0e68f
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-03-19 18:17:24 +0530

    Spring boot profile example initial commit

---
+++ springboot-profile/src/test/java/com/poc/profile/springbootprofile/SpringbootProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.profile.springbootprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
